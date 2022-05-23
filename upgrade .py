ef92a51f5ed1b597aad0de7aab106804433b0ade
L.H <michael123303@yahoo.com>
2022-05-24 00:43:35 +0800

    Update upgrade .py

--- upgrade .py
+++ upgrade .py
@@ -1,7 +1,7 @@
 import pkg_resources
 from subprocess import call
 
-print(''.join("Start Upgrade\n"))
+print(''.join("Start Upgrade.\n"))
 packages = [dist.project_name for dist in pkg_resources.working_set]
 call("pip install --upgrade " + ' '.join(packages), shell=True)
-print(''.join("\n\nUpgrade Finish"))
+print(''.join("\n\nUpgrade Completed."))
